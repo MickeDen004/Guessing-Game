91bf0ad4f2995fbb075b5df32ca8aa786f5be402
nikden34562 <nikolasmongrail@gmail.com>
2021-11-25 18:28:01 +0300

    Guessing game Git hub

--- Guessing game #1.py
+++ Guessing game #1.py
@@ -4,13 +4,7 @@ guessing game for github
 
 name = input("Hi! What's your name?")
 n1 = 47
-while True:
-    guess = int(input(f'Ok. Print your guess from 1 to 100, {name}'))
-    if 0 <guess < 101:
-        break
-    else:
-        print('Try again.')
-        continue
+guess = int(input(f'Ok. Print your guess from 1 to 100, {name}'))
 win = guess == n1
 print("You win!"*win + \
       "I win"*(1-win)
