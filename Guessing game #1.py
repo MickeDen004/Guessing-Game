0c454b1a998ad65bc947aea0349928bb2416ab9b
nikden34562 <94785058+nikden34562@users.noreply.github.com>
2021-11-28 18:53:25 +0300

    Add files via upload

--- Guessing game #1.py
+++ Guessing game #1.py
@@ -1,12 +1,14 @@
 """
 guessing game for github 
 """
-
+import random
 name = input("Hi! What's your name?")
-n1 = 47
+computer_number = random.randint(1,100)
 guess = int(input(f'Ok. Print your guess from 1 to 100, {name}'))
-win = guess == n1
+win = guess == computer_number
 print("You win!"*win + \
       "I win"*(1-win)
       )
+print("Thank you!")
+print(f'My nuber was {computer_number}')
 
